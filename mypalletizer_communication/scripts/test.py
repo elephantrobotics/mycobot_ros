7f56df9752221a5586c343d28aca1bef00bf942e
wangWking <842749351@qq.com>
2022-04-07 17:16:30 +0800

    delete remote directory mypalletizer_communication

--- mypalletizer_communication/scripts/test.py
+++
@@ -1,7 +0,0 @@
-from pymycobot.mypalletizer import MyPalletizer
-mp = MyPalletizer("/dev/ttyUSB0", 115200)
-n = mp.get_angles()
-# while(1):
-#     print(mp.get_angles())
-    # print(mp.get_radians())
-mp.send_angles([100,0,0,150],30)
