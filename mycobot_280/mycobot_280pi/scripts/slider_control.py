6598c87e494c08f7fd4da47fdb02fb126f0c1c42
wangWking <842749351@qq.com>
2022-06-09 16:06:43 +0800

    add mycobot_280pi ROS

---
+++ mycobot_280/mycobot_280pi/scripts/slider_control.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python2
+# encoding=utf-8
+
+"""[summary]
+This file obtains the joint angle of the manipulator in ROS,
+and then sends it directly to the real manipulator using `pymycobot` API.
+This file is [slider_control.launch] related script.
+Passable parameters:
+    port: serial prot string. Defaults is '/dev/ttyAMA0'
+    baud: serial prot baudrate. Defaults is 1000000.
+"""
+
+import rospy
+from sensor_msgs.msg import JointState
+
+from pymycobot.mycobot import MyCobot
+
+
+mc = None
+
+
+def callback(data):
+    # rospy.loginfo(rospy.get_caller_id() + "%s", data.position)
+    print(data.position)
+    data_list = []
+    for index, value in enumerate(data.position):
+        data_list.append(value)
+
+    mc.send_radians(data_list, 80)
+    # time.sleep(0.5)
+
+
+def listener():
+    global mc
+    rospy.init_node("control_slider", anonymous=True)
+
+    rospy.Subscriber("joint_states", JointState, callback)
+    port = rospy.get_param("~port", "/dev/ttyAMA0")
+    baud = rospy.get_param("~baud", 1000000)
+    print(port, baud)
+    mc = MyCobot(port, baud)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    # spin()只是阻止python退出，直到该节点停止
+    print("spin ...")
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    listener()
