7c871104e857943f2004dd5e31beadf6512c13a4
2929ss <1814754095@qq.com>
2022-09-15 17:08:03 +0800

    update

--- mycobot_ai/ai_mecharm_270/scripts/test.py
+++ mycobot_ai/ai_mecharm_270/scripts/test.py
@@ -6,8 +6,8 @@ from pymycobot.genre import Angle
 from pymycobot import PI_PORT, PI_BAUD  # 当使用树莓派版本的mycobot时，可以引用这两个变量进行MyCobot初始化
 import time,os
 
-mc = MyCobot(os.popen("ls /dev/ttyUSB*").readline()[:-1], 115200)
-# mc = MyCobot(os.popen("ls /dev/ttyACM*").readline()[:-1], 115200)
+# mc = MyCobot(os.popen("ls /dev/ttyUSB*").readline()[:-1], 115200)
+mc = MyCobot(os.popen("ls /dev/ttyACM*").readline()[:-1], 115200)
 
 # mc = MyCobot("/dev/ttyAMA0", 1000000)
 # mc = MyCobot(PI_PORT,PI_BAUD)
@@ -16,17 +16,17 @@ mc = MyCobot(os.popen("ls /dev/ttyUSB*").readline()[:-1], 115200)
 # mc.send_angles([0,0,0,0,90,0],20) # coords:[95.6, 0.5, 166.4, 179.12, -0.18, 179.46]
 # time.sleep(4)
 
-mc.send_angle(3,-15,30)
+# mc.send_angle(3,-15,30)
 
 # mc.send_angles([-3.25, 17.22, -32.51, 2.37, 92.54, -36.21],30)
 
 # mc.send_coords([92.3, -104.9, 211.4, -179.6, 28.91, 131.29], 30, 0) # above the red bucket
 # time.sleep(4)
 
-# mc.send_coords([165.0, -93.6, 201.4, -173.43, 46.23, 160.65],30,0) # above the green bucket
-# time.sleep(4)
+mc.send_coords([169.2, -96.4, 198.0, -173.06, 50.9, 159.3],30,0) # above the green bucket
+time.sleep(4)
 
-# mc.send_coords([88.1, 126.3, 193.4, 162.15, 2.23, 156.02],30,0) # above the blue bucket
+# mc.send_coords([71.7, 117.2, 218.6, 151.82, 11.3, 156.74],30,0) # above the blue bucket
 # time.sleep(4)
 
 # mc.send_coords([-5.4, 120.6, 204.6, 162.66, -6.96, 159.93],30,0) # above the gray bucket
