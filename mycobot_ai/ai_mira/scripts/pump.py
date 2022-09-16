fe0eee2bcc58168af6020e0f86fc42dcee20380d
weijian <weijian.wang@elephantrobotics.com>
2022-09-16 14:27:43 +0800

    add mira aikit

---
+++ mycobot_ai/ai_mira/scripts/pump.py
@@ -0,0 +1,23 @@
+#encoding: UTF-8
+#!/usr/bin/env python2
+
+import rospy
+import time
+from moving_utils import Movement
+
+class Pump(Movement):
+
+	def __init__(self):
+		super(Pump, self).__init__()
+		rospy.init_node("pump", anonymous=True)
+
+	def run(self):
+		# self.pub_pump(True)
+		# time.sleep(1)
+		self.pub_pump(True)
+		time.sleep(5)
+		self.pub_pump(False)
+
+if __name__ == "__main__":
+	pump = Pump()
+	pump.run()
