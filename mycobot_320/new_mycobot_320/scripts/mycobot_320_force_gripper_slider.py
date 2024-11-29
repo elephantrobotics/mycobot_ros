83fc7db66dc964330c60fdcc21b55db33c96fb03
wangWking <842749351@qq.com>
2024-11-29 13:06:54 +0800

    320M5/PI增加力控夹爪ros1 moveit、320PI版本增加Pro自适应夹爪demo

--- mycobot_320/new_mycobot_320/scripts/mycobot_320_force_gripper_slider.py
+++ mycobot_320/new_mycobot_320/scripts/mycobot_320_force_gripper_slider.py
@@ -13,7 +13,19 @@ import rospy
 import time
 from sensor_msgs.msg import JointState
 import math
-from pymycobot.mycobot320 import MyCobot320
+import pymycobot
+from packaging import version
+
+# min low version require
+MIN_REQUIRE_VERSION = '3.6.4'
+
+current_verison = pymycobot.__version__
+print('current pymycobot library version: {}'.format(current_verison))
+if version.parse(current_verison) < version.parse(MIN_REQUIRE_VERSION):
+    raise RuntimeError('{}The version of pymycobot library must be greater than {} or higher. The current version is {}. Please upgrade the library version.'.format(MIN_REQUIRE_VERSION, current_verison))
+else:
+    print('pymycobot library version meets the requirements!')
+    from pymycobot.mycobot320 import MyCobot320
 
 
 mc = None
