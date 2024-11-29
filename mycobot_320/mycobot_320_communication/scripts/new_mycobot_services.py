88c8f0e44864225a902480c73252145f493c180d
wangWking <842749351@qq.com>
2024-11-29 17:42:35 +0800

    2022款320适配新版pymycobot库，2020款320提示使用指定范围版本

--- mycobot_320/mycobot_320_communication/scripts/new_mycobot_services.py
+++ mycobot_320/mycobot_320_communication/scripts/new_mycobot_services.py
@@ -10,15 +10,15 @@ import pymycobot
 from packaging import version
 
 # min low version require
-MAX_REQUIRE_VERSION = '3.6.0'
+MIN_REQUIRE_VERSION = '3.6.0'
 
 current_verison = pymycobot.__version__
 print('current pymycobot library version: {}'.format(current_verison))
-if version.parse(current_verison) > version.parse(MAX_REQUIRE_VERSION):
-    raise RuntimeError('{}The version of pymycobot library must be less than {} . The current version is {}. Please downgrade the library version.'.format(MAX_REQUIRE_VERSION, current_verison))
+if version.parse(current_verison) < version.parse(MIN_REQUIRE_VERSION):
+    raise RuntimeError('{}The version of pymycobot library must be greater than {} or higher. The current version is {}. Please upgrade the library version.'.format(MIN_REQUIRE_VERSION, current_verison))
 else:
     print('pymycobot library version meets the requirements!')
-    from pymycobot.mycobot import MyCobot
+    from pymycobot.mycobot320 import MyCobot320
 
 mc = None
 
@@ -66,7 +66,7 @@ def create_handle():
     port = rospy.get_param("~port")
     baud = rospy.get_param("~baud")
     rospy.loginfo("%s,%s" % (port, baud))
-    mc = MyCobot(port, baud)
+    mc = MyCobot320(port, baud)
 
 
 def create_services():
--- mycobot_320/new_mycobot_320/scripts/mycobot_320_slider.py
+++ mycobot_320/new_mycobot_320/scripts/mycobot_320_slider.py
@@ -13,7 +13,19 @@ import time
 import rospy
 from sensor_msgs.msg import JointState
 
-from pymycobot.mycobot import MyCobot
+import pymycobot
+from packaging import version
+
+# min low version require
+MIN_REQUIRE_VERSION = '3.6.0'
+
+current_verison = pymycobot.__version__
+print('current pymycobot library version: {}'.format(current_verison))
+if version.parse(current_verison) < version.parse(MIN_REQUIRE_VERSION):
+    raise RuntimeError('{}The version of pymycobot library must be greater than {} or higher. The current version is {}. Please upgrade the library version.'.format(MIN_REQUIRE_VERSION, current_verison))
+else:
+    print('pymycobot library version meets the requirements!')
+    from pymycobot.mycobot320 import MyCobot320
 
 
 mc = None
@@ -37,7 +49,7 @@ def listener():
     port = rospy.get_param("~port", "/dev/ttyACM0")
     baud = rospy.get_param("~baud", 115200)
     print(port, baud)
-    mc = MyCobot(port, baud)
+    mc = MyCobot320(port, baud)
     time.sleep(0.05)
     mc.set_fresh_mode(1)
     time.sleep(0.05)
