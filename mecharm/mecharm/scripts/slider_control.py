a586193c51c2527ee950e20143b682deacdabfcb
wangWking <842749351@qq.com>
2026-09-04 17:07:13 +0800

    perf(270): opt 270 slilder function

--- mecharm/mecharm/scripts/slider_control.py
+++ mecharm/mecharm/scripts/slider_control.py
@@ -30,24 +30,29 @@ else:
 
 
 mc = None
+_last_angles = None
 
 
 def callback(data):
+    global _last_angles
     # rospy.loginfo(rospy.get_caller_id() + "%s", data.position)
     data_list = []
     for index, value in enumerate(data.position):
         radians_to_angles = round(math.degrees(value), 2)
         data_list.append(radians_to_angles)
-        
+    
+    if data_list == _last_angles:
+        return
+    _last_angles = data_list
+    
     rospy.loginfo(rospy.get_caller_id() + "%s", data_list)
-    mc.send_angles(data_list, 25)
+    mc.send_angles(data_list, 25, _async=True)
 
 
 def listener():
     global mc
     rospy.init_node("control_slider", anonymous=True)
 
-    rospy.Subscriber("joint_states", JointState, callback)
     port = rospy.get_param("~port", "/dev/ttyUSB0") 
     baud = rospy.get_param("~baud", 115200)
     print(port, baud)
@@ -55,7 +60,7 @@ def listener():
     time.sleep(0.05)
     mc.set_fresh_mode(1)
     time.sleep(0.05)
-
+    rospy.Subscriber("joint_states", JointState, callback, queue_size=1)
     # spin() simply keeps python from exiting until this node is stopped
     # spin() 只是阻止python退出，直到该节点停止
     print("spin ...")
